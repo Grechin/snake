b3c90f1b7afb0e57e57e0b7f7a3a1cf44c65a6a9
Grechin <grechin.au@yandex.ru>
2015-11-07 21:37:04 +0300

    добавлен класс VerticalLine и Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,7 +11,7 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1,3,'*');
+            /*Point p1 = new Point(1,3,'*');
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
@@ -20,7 +20,29 @@ namespace Snake
             HorizontalLine line = new HorizontalLine(5,10,8,'+');
             line.Drow();
 
-            Console.ReadLine();          
+            VerticalLine line1 = new VerticalLine(15, 20, 5, '*');
+            line1.Drow();
+
+            Console.ReadLine();  */
+
+            Console.SetBufferSize(80,25);
+
+            //Отрисовка рамочки
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            upline.Drow();
+            downline.Drow();
+            leftline.Drow();
+            rightline.Drow();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
+            Console.ReadLine();
+
         }
     }
 }
